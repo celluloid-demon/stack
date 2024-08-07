358bc8975d70d251f9bf9dea67ab56c2d3784e85
jonathan <celluloid-demon@users.noreply.github.com>
2024-08-06 18:40:17 -0700

    added install-openmediavault.sh

---
+++ install-openmediavault.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# script that installs omv-extras and openmediavault if not installed already
+sudo wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash
