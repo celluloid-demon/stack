520052605b9fabc3034c711141922479f42343d3
jonathan <celluloid-demon@users.noreply.github.com>
2024-08-07 02:28:23 -0700

    bootstrap initial commit

---
+++ install/bootstrap/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# script that installs and configures basic packages, idempotently
+sudo wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash
