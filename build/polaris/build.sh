d4f1d89d37e0faccc3caaa83341925e636d5ca70
jonathan <celluloid-demon@users.noreply.github.com>
2024-08-29 00:03:44 -0700

    versioned docker images

--- build/polaris/build.sh
+++ build/polaris/build.sh
@@ -6,7 +6,8 @@ WORKDIR="$(dirname "$0")/build" && \
 mkdir -p "$WORKDIR" && \
 cd "$WORKDIR"
 
-git clone "https://github.com/ogarcia/docker-polaris/tree/1e6b9414f251a118cdcf94cbe8a1bc4796b85486" && \
-cd docker-polaris
+git clone "https://github.com/ogarcia/docker-polaris/" && \
+cd docker-polaris && \
+git checkout 1e6b9414f251a118cdcf94cbe8a1bc4796b85486
 
 make
