e94f3f9dfc728ccc3cbda7677ade9f3e530aa080
jonathan <celluloid-demon@users.noreply.github.com>
2024-10-21 18:10:31 -0700

    tweak

--- helper-scripts/docker/create-nfs-volume.sh
+++ helper-scripts/docker/create-nfs-volume.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-docker volume create \
-    --driver local \ 
-    --opt type=nfs \ 
-    --opt o=addr=192.168.1.171,rw \ 
-    --opt device=:/mnt/pool/global \ 
-    nfs-test
+docker volume create --driver local --opt type=nfs --opt o=addr=192.168.1.171,rw --opt device=:/mnt/pool/global nfstest
