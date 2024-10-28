263af2950bb26475d8df1a4d9c2ef0e13b8957cb
jonathan <celluloid-demon@users.noreply.github.com>
2024-10-28 01:22:08 -0700

    tweaks

--- helper-scripts/nfs/test-io.sh
+++ helper-scripts/nfs/test-io.sh
@@ -3,9 +3,10 @@
 # More info: https://nfs.sourceforge.net/nfs-howto/ar01s05.html
 
 readonly OUT='fio-test'
+readonly WORKDIR="$1"
 
 # Test
-fio --name=${OUT} --rw=read --direct=1 --ioengine=libaio --bs=32k --numjobs=4 --iodepth=4 --size=1G --runtime=30 --group_reporting
+fio --name=${OUT} --rw=read --direct=1 --ioengine=libaio --bs=32k --numjobs=4 --iodepth=4 --size=1G --runtime=30 --group_reporting --directory=${WORKDIR}
 
 # Cleanup
-rm ./${OUT}*
+rm "${WORKDIR}/${OUT}"*
