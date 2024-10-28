819153c699a19d873ef86a4c8ef8a3a8861ec911
jonathan <celluloid-demon@users.noreply.github.com>
2024-10-28 01:23:46 -0700

    tweaks

--- helper-scripts/nfs/test-io.sh
+++ helper-scripts/nfs/test-io.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Description: Write 4g for i/o test over NFS.
+
 # More info: https://nfs.sourceforge.net/nfs-howto/ar01s05.html
 
 readonly OUT='fio-test'
 readonly WORKDIR="$1"
 
 # Test
-fio --name=${OUT} --rw=read --direct=1 --ioengine=libaio --bs=32k --numjobs=4 --iodepth=4 --size=1G --runtime=30 --group_reporting --directory=${WORKDIR}
+fio --name=${OUT} --rw=read --direct=1 --ioengine=libaio --bs=32k --numjobs=4 --iodepth=4 --size=1G --runtime=30 --group_reporting --directory="${WORKDIR}"
 
 # Cleanup
 rm "${WORKDIR}/${OUT}"*
