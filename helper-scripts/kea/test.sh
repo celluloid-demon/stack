dfec125a6131e2f626d4e8be348802c581003771
jonathan <celluloid-demon@users.noreply.github.com>
2024-10-30 23:00:02 -0700

    tweak

--- helper-scripts/kea/test.sh
+++ helper-scripts/kea/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Description: Make test-query to kea-dhcp4 server via its control agent.
+# Description: Make test-query to kea-dhcp4 service via its control agent.
 
 curl -X POST -H "Content-Type: application/json" \
     -d '{ "command": "config-get", "service": [ "dhcp4" ] }' \
