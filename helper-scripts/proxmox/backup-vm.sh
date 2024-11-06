cc5e3e310fe8c56038f377ca407bfedd0a8435d5
jonathan <celluloid-demon@users.noreply.github.com>
2024-11-05 22:10:26 -0800

    tweaks

--- helper-scripts/proxmox/backup-vm.sh
+++ helper-scripts/proxmox/backup-vm.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+##########################################################################################
+#                                                                                        #
+#          !!WARNING: DO NOT TARGET NFS SHARE FROM SAME NODE AS TRUENAS GUEST!!          #
+#                                                                                        #
+##########################################################################################
+
 readonly VM_ID=$1
+readonly LIMIT=5000 # KiB/s (default: unlimited)
 readonly BACKUP_DIR='/mnt/pve/global/dump'
 
+# WARNING: Without BW limits, backup will always consume the max bandwidth. So
+# if your backup server is on the same server, and the VMs etc, then this
+# situation will happen (vzdump freezing the host).
+
+# vzdump $VM_ID --bwlimit $LIMIT --dumpdir $BACKUP_DIR --mode snapshot
 vzdump $VM_ID --dumpdir $BACKUP_DIR --mode snapshot
