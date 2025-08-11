f6b95bca164fac4b81710111f4172c1ebfe55f49
jonathan <celluloid-demon@users.noreply.github.com>
2025-08-10 17:02:09 -0700

    added flash utility

---
+++ cron/flash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT='./sync-ix-apps.sh'
+POOL='/mnt/lake/pool.6d86b5884ec6e9d1de8a908766de61c5'
+
+BIN="${POOL}/bin"
+
+cp "$SCRIPT" "${BIN}/"
--- cron/sync-ix-apps.sh
+++ cron/sync-ix-apps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Note: Recommended to save cron scripts to bin folder in pool or user home folder
+
 SOURCE='/mnt/.ix-apps'
 DESTINATION='/mnt/lake/pool.6d86b5884ec6e9d1de8a908766de61c5/backup/apps'
 
